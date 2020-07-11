9c7391e75a96af25a9386bc391e11b52dd68df40
Mario Cecchi <macecchi@gmail.com>
2020-07-11 19:40:39 -0300

    Remove App tests temporarily

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders categories link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/categorias/i);
-  expect(linkElement).toBeInTheDocument();
-});
