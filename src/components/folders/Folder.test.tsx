feaeb74deb8764447190e1a276410f39021aa92e
Lucas Hanke <lucas_hanke@external.mckinsey.com>
2020-07-15 19:15:14 -0300

    use screen on react tests

--- src/components/folders/Folder.test.tsx
+++ src/components/folders/Folder.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElementToBeRemoved, act } from '@testing-library/react';
+import { render, waitForElementToBeRemoved, act, screen } from '@testing-library/react';
 import FolderComponent from './Folder';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
@@ -8,29 +8,29 @@ jest.mock('../../aws/config');
 
 describe('<Folder>', () => {
   it('displays a loading message and eventually shows all the subfolders', async () => {
-    const { getByText, getByRole } = render(<MemoryRouter><FolderComponent /></MemoryRouter>);
-    const loading = getByText(/loading/i);
+    render(<MemoryRouter><FolderComponent /></MemoryRouter>);
+    const loading = screen.getByText(/loading/i);
     expect(loading).toBeInTheDocument();
 
-    await waitForElementToBeRemoved(() => getByText(/loading/i));
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
 
-    expect(getByRole('heading')).toHaveTextContent('Categorias');
-    expect(getByText(/Category 1/i)).toBeInTheDocument();
-    expect(getByText(/Category 2/i)).toBeInTheDocument();
-    expect(getByText(/Category 3/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent('Categorias');
+    expect(screen.getByText(/Category 1/i)).toBeInTheDocument();
+    expect(screen.getByText(/Category 2/i)).toBeInTheDocument();
+    expect(screen.getByText(/Category 3/i)).toBeInTheDocument();
   });
 
   it('loads the subfolder when clicking', async () => {
-    const { getByRole, findByText, getByText } = render(<MemoryRouter><FolderComponent /></MemoryRouter>);
+    render(<MemoryRouter><FolderComponent /></MemoryRouter>);
 
     await act(async () => {
-      const categoryFolder = await findByText(/Category 1/i);
+      const categoryFolder = await screen.findByText(/Category 1/i);
       categoryFolder.click();
     });
 
-    expect(getByRole('heading')).toHaveTextContent('Category 1');
-    expect(getByText(/SubCategory 11/i)).toBeInTheDocument();
-    expect(getByText(/SubCategory 12/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent('Category 1');
+    expect(screen.getByText(/SubCategory 11/i)).toBeInTheDocument();
+    expect(screen.getByText(/SubCategory 12/i)).toBeInTheDocument();
   });
 
   it.each([
@@ -39,34 +39,34 @@ describe('<Folder>', () => {
     ['/Category 1/', 'Category 1', ['SubCategory 11', 'SubCategory 12']],
     ['/Category 1/SubCategory 11', 'SubCategory 11', []],
   ])('loads the folder with the correct prefix when page url is %s', async (pageUrl, expectedHeader, expectedSubFolders) => {
-    const { getByText, getByRole } = render(<MemoryRouter initialEntries={[pageUrl]}><FolderComponent /></MemoryRouter>);
+    render(<MemoryRouter initialEntries={[pageUrl]}><FolderComponent /></MemoryRouter>);
 
-    await waitForElementToBeRemoved(() => getByText(/loading/i));
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
 
-    expect(getByRole('heading')).toHaveTextContent(expectedHeader);
+    expect(screen.getByRole('heading')).toHaveTextContent(expectedHeader);
     expectedSubFolders.forEach((expectedSubFolder) => {
-      const subfolder = getByText(expectedSubFolder);
+      const subfolder = screen.getByText(expectedSubFolder);
       expect(subfolder).toBeInTheDocument();
       expect(subfolder).toHaveAttribute('href');
     });
   });
 
   it('loads the photo gallery when folder has contents', async () => {
-    const { getByText, getAllByRole } = render(<MemoryRouter initialEntries={['/Category 1/SubCategory 11']}><FolderComponent /></MemoryRouter>);
+    render(<MemoryRouter initialEntries={['/Category 1/SubCategory 11']}><FolderComponent /></MemoryRouter>);
 
-    await waitForElementToBeRemoved(() => getByText(/Loading/i));
+    await waitForElementToBeRemoved(() => screen.getByText(/Loading/i));
 
-    expect(getAllByRole('img')[0]).toHaveAttribute('src', expect.stringContaining('Category 1/SubCategory 11/photo111.jpg'));
+    expect(screen.getAllByRole('img')[0]).toHaveAttribute('src', expect.stringContaining('Category 1/SubCategory 11/photo111.jpg'));
   });
 
   it('sets the hash of the photo on url when navigating the photo gallery', async () => {
     const history = createMemoryHistory();
     history.push('/Category 2/SubCategory 22');
-    const { getByText, getAllByRole } = render(<Router history={history}><FolderComponent /></Router>);
+    render(<Router history={history}><FolderComponent /></Router>);
 
-    await waitForElementToBeRemoved(() => getByText(/Loading/i));
+    await waitForElementToBeRemoved(() => screen.getByText(/Loading/i));
 
-    getAllByRole('img')[4].click();
+    screen.getAllByRole('img')[4].click();
 
     expect(history.location.hash).toEqual('#photo222.jpg');
   });
